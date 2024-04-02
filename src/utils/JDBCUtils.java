1a902be46c993d91876aae32f9f80dd5ed14fb35
YU_lag <2161334147@qq.com>
2024-04-02 16:24:19 +0800

    添加默认文本和账号与密码框

---
+++ src/utils/JDBCUtils.java
@@ -0,0 +1,71 @@
+package utils;
+
+import com.alibaba.druid.pool.DruidDataSourceFactory;
+
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
+
+public class JDBCUtils {
+    private static DataSource dataSource;
+    static {
+        try {
+            Properties properties=new Properties();
+            properties.load(JDBCUtils.class.getClassLoader().getResourceAsStream("utils\\databsae.properties"));
+            dataSource = DruidDataSourceFactory.createDataSource(properties);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static Connection getConnection() throws SQLException {
+        return dataSource.getConnection();
+    }
+
+    public static void close(Statement statement,Connection connection){
+        if(statement!=null){
+            try {
+                statement.close();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        if(connection!=null){
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    public static void close(Statement statement, Connection connection, ResultSet resultSet){
+        if(statement!=null){
+            try {
+                statement.close();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        if(connection!=null){
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        if(resultSet!=null){
+            try {
+                resultSet.close();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
