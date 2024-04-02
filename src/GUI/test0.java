379d9f5b71ef625b3d771e47f0932d4be22583c2
YU_lag <2161334147@qq.com>
2024-04-02 15:41:30 +0800

    添加默认文本和账号与密码框

--- src/GUI/test0.java
+++ src/GUI/test0.java
@@ -25,11 +25,15 @@ public class test0 extends JFrame {
     private static JPanel inputPanel; // 用于放置文本的面板
     private JPanel gapPanel; // 用于创建间隔的面板
     private static JScrollPane scrollPane;
+    private JLabel usernameLabel; // 账号标签
+    private JLabel passwordLabel; // 密码标签
     private JLabel numberLabel; // 工号标签
     private JLabel nameLabel; // 姓名标签
     private JLabel genderLabel; // 性别标签
     private JLabel deptLabel; // 部门标签
     private JLabel timeLabel; // 入职年份标签
+    private static JTextField usernameText; // 账号框
+    private static JTextField passwordText; // 密码框
 
     private static JTextField numberText; // 工号框
     private static JTextField nameText; // 姓名框
@@ -67,12 +71,16 @@ public class test0 extends JFrame {
         buttonPanel = new JPanel(); // 创建一个面板用于放置按钮
 
         // 增删改文本框
+        usernameLabel = new JLabel(("账号:"));
+        passwordLabel = new JLabel("密码:");
         numberLabel = new JLabel("工号:");
         nameLabel = new JLabel("姓名:");
         genderLabel = new JLabel("性别:");
         deptLabel = new JLabel("部门:");
         timeLabel = new JLabel("入职年份:");
 
+        usernameText = new JTextField(15);
+        passwordText = new JTextField(15);
         numberText = new JTextField(15);
         nameText = new JTextField(15);
         genderText = new JTextField(15);
@@ -93,6 +101,7 @@ public class test0 extends JFrame {
         // 使用 BorderLayout 来放置表格和按钮面板
         scrollPane = new JScrollPane(table);
         scrollPane.setPreferredSize(new Dimension(500, 300));
+        add(scrollPane);
 
         // 设置按钮面板的布局为 BoxLayout
         buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.Y_AXIS));
@@ -101,19 +110,25 @@ public class test0 extends JFrame {
 
 
         inputPanel.setLayout(null);
-        numberLabel.setBounds(50, 10, 100, 25);
-        nameLabel.setBounds(50, 70, 100, 25);
-        genderLabel.setBounds(50, 130, 100, 25);
-        deptLabel.setBounds(50, 190, 100, 25);
-        timeLabel.setBounds(50, 250, 100, 25);
+        usernameLabel.setBounds(50,10,100,25);
+        passwordLabel.setBounds(50,70,100,25);
+        numberLabel.setBounds(350, 10, 100, 25);
+        nameLabel.setBounds(350, 70, 100, 25);
+        genderLabel.setBounds(350, 130, 100, 25);
+        deptLabel.setBounds(350, 190, 100, 25);
+        timeLabel.setBounds(350, 250, 100, 25);
 
-        numberText.setBounds(125, 10, 200, 25);
-        nameText.setBounds(125, 70, 200, 25);
-        genderText.setBounds(125, 130, 200, 25);
-        deptText.setBounds(125, 190, 200, 25);
-        timeText.setBounds(125, 250, 200, 25);
-
-        add(scrollPane);
+        usernameText.setBounds(125,10,200,25);
+        usernameText.addFocusListener(new JTextFieldHintListener(usernameText, "账号默认与工号相同"));
+        passwordText.setBounds(125,70,200,25);
+        passwordText.addFocusListener(new JTextFieldHintListener(passwordText, "密码默认123456"));
+        numberText.setBounds(425, 10, 200, 25);
+        numberText.addFocusListener(new JTextFieldHintListener(numberText, "不输入按最后一位工号递增"));
+        nameText.setBounds(425, 70, 200, 25);
+        genderText.setBounds(425, 130, 200, 25);
+        deptText.setBounds(425, 190, 200, 25);
+        timeText.setBounds(425, 250, 200, 25);
+        timeText.addFocusListener(new JTextFieldHintListener(timeText, "不输入默认当前年份"));
     }
 
     private void addComponents() {
@@ -130,6 +145,12 @@ public class test0 extends JFrame {
     }
 
     private void addInput() {
+        inputPanel.add(usernameLabel);
+        inputPanel.add(usernameText);
+
+        inputPanel.add(passwordLabel);
+        inputPanel.add(passwordText);
+
         inputPanel.add(numberLabel);
         inputPanel.add(numberText);
 
@@ -181,15 +202,14 @@ public class test0 extends JFrame {
         // 例如删除、更新和查询功能的实现
         // 这里省略了具体的事件处理代码，需要根据实际需求来编写
     }
+    public static void main(String[] args) {
+        // 创建窗口实例并显示
+        test0 crudInterface = new test0();
+        crudInterface.setTitle("管理员增删改查界面");
+        crudInterface.setSize(1000, 500); // 调整窗口大小以适应组件
+        crudInterface.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        crudInterface.setLocationRelativeTo(null);
+
+        crudInterface.setVisible(true);
+    }
 }
-//    public static void main(String[] args) {
-//        // 创建窗口实例并显示
-//        test0 crudInterface = new test0();
-//        crudInterface.setTitle("管理员增删改查界面");
-//        crudInterface.setSize(1000, 500); // 调整窗口大小以适应组件
-//        crudInterface.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//        crudInterface.setLocationRelativeTo(null);
-//
-//        crudInterface.setVisible(true);
-//    }
-//}
